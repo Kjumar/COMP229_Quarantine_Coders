83f14b2be9a833febe5faab4255f03d38711b0b5
TeeJayWilliamson <71295868+TeeJayWilliamson@users.noreply.github.com>
2020-11-13 16:38:21 -0500

    Updated Routes to coincide with Controllers

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,20 @@
 let express = require('express');
 let router = express.Router();
 
+let indexController = require('../controllers/index');
+
+/* GET  HOME page. */
+router.get('/', indexController.displayHomePage);
+
+/* GET  HOME page. */
+router.get('/home', indexController.displayHomePage);
+
+/* GET ABOUT US page. */
+router.get('/about', indexController.displayAboutPage);
+
+/* GET CONTACT US page. */
+router.get('/contact', indexController.displayContactPage);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
