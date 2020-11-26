b5110de6ad7d9fbcaaa32a914004067ae7cf9db6
Kjumar <jganguli@hotmail.ca>
2020-11-26 17:06:12 -0500

    switch to live database

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/Quarantine-Coders"
-    //"URI": "mongodb+srv://jiaxing:ym9EI2W895W3GrID@1027.ja9zx.mongodb.net/Quarantine_Coders?retryWrites=true&w=majority",
+    //"URI": "mongodb://localhost/Quarantine-Coders"
+    "URI": "mongodb+srv://jiaxing:ym9EI2W895W3GrID@1027.ja9zx.mongodb.net/Quarantine_Coders?retryWrites=true&w=majority",
 }
